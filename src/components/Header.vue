2f3f5c6b30864b3f1c026fb403d3502fd1bb495c
Darren Tarrant <in@djtarrant.co.uk>
2020-08-17 19:55:37 +0100

    Set up components

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="header">
+    Links go here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style>
+#header {
+}
+</style>
---
+++ src/components/IsVegan.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="isVegan">
+    <h1>Is Vegan Vue App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IsVegan',
+  components: {
+  },
+  data(){
+      return(
+          foodItems=[]
+      )
+  }
+}
+</script>
+
+<style>
+#isVegan {
+}
+</style>
